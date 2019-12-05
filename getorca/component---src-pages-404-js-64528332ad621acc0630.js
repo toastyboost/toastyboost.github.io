9fc88bbda3e0ecc176758f42dbd42ff7c1b63d0d
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2019-12-05 04:15:39 +0300

    update orca landing

---
+++ getorca/component---src-pages-404-js-64528332ad621acc0630.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,t,e){"use strict";e.r(t);var u=e("q1tI"),c=e("Wbzz");t.default=function(){return u.useEffect((function(){Object(c.navigate)("/en/")}),[]),null}}}]);
