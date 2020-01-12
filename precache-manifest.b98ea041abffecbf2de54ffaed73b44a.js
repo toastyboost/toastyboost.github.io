26e98696df96a9960620ba1d23e4ad829fd10b49
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2020-01-12 21:48:40 +0300

    add chat

---
+++ precache-manifest.b98ea041abffecbf2de54ffaed73b44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468df0f3c4c986009a005c63e27871d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "338c206b3b0aa932d61f",
+    "url": "/static/js/2.fccc02fd.chunk.js"
+  },
+  {
+    "revision": "2ba2081b6c7df8ecdcca12487b4ce022",
+    "url": "/static/js/2.fccc02fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "982d16cbacc941324ad0",
+    "url": "/static/js/main.f2fc1b1e.chunk.js"
+  },
+  {
+    "revision": "cafec88b0d58ce2134b6",
+    "url": "/static/js/runtime-main.0372c36d.js"
+  }
+]);
